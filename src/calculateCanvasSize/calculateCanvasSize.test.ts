87ed0c3bfa0c353c76c6336cf8d9db1b099c8c33
Wesley <wesley@wesleyadu.com>
2025-06-14 20:28:14 -0400

    test: should return zero when length is zero

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -5,5 +5,14 @@ describe("calculateCanvasSize", () => {
     const result = calculateCanvasSize("10", "100");
 
     expect(result).toEqual(1000);
-  })
+  });
+
+  test("should return zero when length is zero", () => {
+    const length = "0";
+    const width = "10";
+
+    const result = calculateCanvasSize(length, width);
+
+    expect(result).toBe(0);
+  });
 });
